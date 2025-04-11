e5764dbff9012336dfdcdb21a19409fdae964f1e
adjelritmo <renionapp@gmail.com>
2025-04-11 10:41:05 +0100

    kkkk

--- src/components/timeUp.js
+++ src/components/timeUp.js
@@ -6,12 +6,10 @@ const TimeUp = (props) => {
 
     return (
         <View style={{ width: '100%', alignItems: 'center', justifyContent: 'center', flex: 1 }}>
-
             <View style={timeUpStyles.view}>
                 <Text style={timeUpStyles.title}>Tempo Esgotado</Text>
                 <Ionicons name="stopwatch" size={200} color="#0dcaf0" />
             </View>  
-
             <View style={{ width: '100%', alignItems: 'center', justifyContent: 'center' }}>
                 <TouchableOpacity onPress={() => props.setTimeUp()} style={{ width: '90%', backgroundColor: props.gameOver ? 'red' : '#0dcaf0', alignItems: 'center', justifyContent: 'center', padding: 10, borderRadius: 10, height: 100 }}>
                     <Text style={{ color: 'white', fontSize: 40, fontWeight: 'bold' }}>{props.gameOver ? 'FIM DO JOGO' : 'PROXIMA'}</Text>
